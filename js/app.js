7077ad3dc034246354a2b8aee61bad2e987eb2dc
Maksim434434 <kinme608@gmail.com>
2025-10-25 10:30:42 +0500

    suma+b

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
-let a = 190
-let b = 424
-console.log(a+b);
+function sum(a,b)
+{
+    return a.value*1 + b.value*1
+}
+
+
+let btn = document.querySelector('section button')
+btn.addEventListener ('click', function (){
+let nums = document.querySelectorAll('section input')
+let rezult = document.querySelector('section p')
+rezult.innerHTML = 'сумма:' + sum(nums[0], nums[1])
+
+})
